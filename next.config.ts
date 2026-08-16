5a706cd56e99f7bd0787ba2595774549e8ad79a9
Ruben-D55 <rd9968457@gmail.com>
2026-08-16 15:38:48 -0400

    Finalizar backend y configuración de producción

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,1 @@
-import type{NextConfig}from"next";const config:NextConfig={reactStrictMode:true};export default config;
+import type{NextConfig}from"next";const config:NextConfig={reactStrictMode:true,output:"standalone",poweredByHeader:false,compress:true};export default config;
