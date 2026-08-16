cedcf6459e42b79e66d8bf9da24ad77346fd2d6b
Ruben-D55 <rd9968457@gmail.com>
2026-08-16 16:37:17 -0400

    Configurar SQL Server con autenticación de Windows

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,1 @@
-import type{NextConfig}from"next";const config:NextConfig={reactStrictMode:true,output:"standalone",poweredByHeader:false,compress:true};export default config;
+import type{NextConfig}from"next";const config:NextConfig={reactStrictMode:true,output:"standalone",poweredByHeader:false,compress:true,serverExternalPackages:["mssql","msnodesqlv8"]};export default config;
